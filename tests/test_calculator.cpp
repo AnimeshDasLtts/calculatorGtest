04e785328949281a0b4d876a945adb77b9452240
AnimeshDasLtts <91518211+AnimeshDasLtts@users.noreply.github.com>
2024-01-23 18:00:34 +0530

    Update test_calculator.cpp

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -1,1 +1,28 @@
+// tests/test_calculator.cpp
+#include "gtest/gtest.h"
+#include "calculator.h"
 
+TEST(Calculator, Add) {
+    Calculator calc;
+    EXPECT_EQ(8, calc.add(5, 3));
+}
+
+TEST(Calculator, Subtract) {
+    Calculator calc;
+    EXPECT_EQ(2, calc.subtract(5, 3));
+}
+
+TEST(Calculator, Multiply) {
+    Calculator calc;
+    EXPECT_EQ(15, calc.multiply(5, 3));
+}
+
+TEST(Calculator, Divide) {
+    Calculator calc;
+    EXPECT_EQ(2, calc.divide(6, 3));
+}
+
+TEST(Calculator, DivideByZero) {
+    Calculator calc;
+    EXPECT_EQ(0, calc.divide(6, 0));
+}
